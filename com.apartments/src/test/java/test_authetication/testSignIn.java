be9a3128b7c79695ccdb3544897e5092db55c884
Dalal-QA <sadouni,dalal@gmail.com>
2023-06-27 10:19:27 -0400

    test case sign in documented

--- com.apartments/src/test/java/test_authetication/testSignIn.java
+++ com.apartments/src/test/java/test_authetication/testSignIn.java
@@ -15,6 +15,16 @@ import java.io.File;
 
 public class testSignIn extends BasePage {
 
+    /* Test Case :Sign In to Apartments.com
+    * 1.Navigate to URl "www.apartments.com"
+    * 2.Click on the "Sign In" button/link.
+    * 3.Enter a valid email address in the email input field
+    * 4.Enter a valid password in the password input field.
+    * 5.Click on the "Sign In" button
+    * 6.Verify that the user is successfully logged in and redirected to the home page.
+    *  */
+
+
     @Test(priority = 1, groups = "smoke")
 
     public void testDoSignIn() {
@@ -22,16 +32,6 @@ public class testSignIn extends BasePage {
         HomePage homePage = new HomePage();
 
         homePage.clickOnSignInLink();
-//        String email= "abc1@gmail.com";
-//        String password="AydenLiam1213";
-
-
-//
-//
-//        webDriverWait.until(ExpectedConditions.visibilityOf(signInPage.emailAddressInput));
-//        signInPage.doSignIn("abc1@gmail.com", "AydenLiam1213");
-
-//
         ExcelData excelData = new ExcelData(BasePage.DATA_PATH);
         SignInPage signInPage = new SignInPage();
         String[][] credentials = excelData.readStringArrays("doSignIn");
@@ -44,16 +44,6 @@ public class testSignIn extends BasePage {
         Assert.assertTrue(checkElementPresent(homePage.userName));
     }
 
-        @DataProvider (name="signupdataprovider")
-        public Object[][] testSignupUserDataProvider()
-        {
-
-            String path= System.getProperty("user.dir")+"\\resources\\test_data.xlsx";
-            ExcelData ex=new ExcelData(path);
-            String data[][]=ex.readStringArrays("doSignIn");
-            return data;
-
-        }
 
 
 
