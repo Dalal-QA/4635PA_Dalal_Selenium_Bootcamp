2c6c815f4a7f80d9217f48f16985eb378124a41a
Dalal-QA <sadouni,dalal@gmail.com>
2023-06-27 13:15:16 -0400

    updates

--- com.apartments/src/test/java/test_authetication/testSignIn.java
+++ com.apartments/src/test/java/test_authetication/testSignIn.java
@@ -16,13 +16,13 @@ import java.io.File;
 public class testSignIn extends BasePage {
 
     /* Test Case :Sign In to Apartments.com
-    * 1.Navigate to URl "www.apartments.com"
-    * 2.Click on the "Sign In" button/link.
-    * 3.Enter a valid email address in the email input field
-    * 4.Enter a valid password in the password input field.
-    * 5.Click on the "Sign In" button
-    * 6.Verify that the user is successfully logged in and redirected to the home page.
-    *  */
+     * 1.Navigate to URl "www.apartments.com"
+     * 2.Click on the "Sign In" button/link.
+     * 3.Enter a valid email address in the email input field
+     * 4.Enter a valid password in the password input field.
+     * 5.Click on the "Sign In" button
+     * 6.Verify that the user is successfully logged in and redirected to the home page.
+     *  */
 
 
     @Test(priority = 1, groups = "smoke")
@@ -44,35 +44,32 @@ public class testSignIn extends BasePage {
         Assert.assertTrue(checkElementPresent(homePage.userName));
     }
 
-
-
-
-    @Test(priority = 1, groups = "smoke", dataProvider ="testDoSignIn")
-
-    public void testDoSignIn(String email, String password) {
-
+    @Test(priority= 2, groups= {"BAT"},dataProvider = "signindataprovider")
+    public void testUserSignIn(String email,String password){
+        SignInPage signInPage = new SignInPage();
         HomePage homePage = new HomePage();
         homePage.clickOnSignInLink();
 
-        //ExcelData excelData = new ExcelData(BasePage.DATA_PATH);
-        SignInPage signInPage = new SignInPage();
-
-        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='username']")));
-        signInPage.doSignIn(email, password);
-
-
+        signInPage.doSignIn(email,password);
         Assert.assertTrue(checkElementPresent(homePage.userName));
     }
 
-        @DataProvider(name = "testDoSignIn")
-        public Object[][] getDoSigInDataProvider(){
-        String path= System.getProperty("user.dir")+ File.separator+ "resources" + File.separator + "test_data.xlsx";;
+    @DataProvider(name="signindataprovider")
+    public Object[][] testSignInUserDataProvider()
+    {
 
-        ExcelData excelData = new ExcelData(path);
-        return excelData.readStringArrays("doSignIn");
+        String path= System.getProperty("user.dir")+"\\resources\\test_data.xlsx";
+        ExcelData ex=new ExcelData(path);
+        String data[][]=ex.readStringArrays("doSignIn");
+        return data;
 
-        }
     }
+
+}
+
+
+
+
 
 
 
