014c032e0314cb0910740144579ed73770ee1aec
Dalal-QA <sadouni,dalal@gmail.com>
2023-07-04 00:33:39 -0400

    test login documented

--- com.investing/src/test/java/test_authentication/TestAuthentication.java
+++ com.investing/src/test/java/test_authentication/TestAuthentication.java
@@ -9,6 +9,15 @@ import utils.ExcelData;
 
 public class TestAuthentication extends BasePage {
 
+    /*Test Case :Login to investing.com
+    *1.Open your web browser and go to the investing.com website.
+    *2.Click on the "Sign In" or "Login" button to proceed to the login page
+    *3.Enter your registered email address in the first input field.
+    *4.Enter your password in the second input field
+    *5.click on the "Login" button to submit the form.
+    * 6.Verify that you are redirected to your account dashboard or the page you were previously viewing before logging in
+     */
+
     @Test(priority= 1, groups= {"BAT"},dataProvider = "loginDataProvider")
 
     public void testLogin(String email,String password){
