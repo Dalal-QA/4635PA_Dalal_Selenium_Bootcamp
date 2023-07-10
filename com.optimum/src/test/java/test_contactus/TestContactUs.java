77074b794e5bd68164767937314d2411bee83ef2
Dalal-QA <sadouni,dalal@gmail.com>
2023-07-10 10:18:15 -0400

    Test documneted

--- com.optimum/src/test/java/test_contactus/TestContactUs.java
+++ com.optimum/src/test/java/test_contactus/TestContactUs.java
@@ -8,6 +8,13 @@ import org.testng.annotations.Test;
 
 public class TestContactUs extends BasePage {
 
+    /*Test Case: Test contact us functionality
+    *1.Navigate to http://optimum.com
+    *2.Click on "Contact us" link at the bottom of the page
+    *3.Click on "Chat Now" button
+    *4. Verify that a small widows appears to start a chat
+     */
+
     @Test(priority = 1,groups={"BAT"})
     public void testContactUs(){
         HomePage homePage=new HomePage();
