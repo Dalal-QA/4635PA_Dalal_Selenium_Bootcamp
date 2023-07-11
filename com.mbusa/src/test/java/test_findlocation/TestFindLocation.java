9535f67fef1f22f08be86638c50813c6d6826ad2
Dalal-QA <sadouni,dalal@gmail.com>
2023-07-10 22:28:32 -0400

    test documented

--- com.mbusa/src/test/java/test_findlocation/TestFindLocation.java
+++ com.mbusa/src/test/java/test_findlocation/TestFindLocation.java
@@ -10,7 +10,16 @@ import utils.ExcelData;
 
 public class TestFindLocation extends BasePage {
 
-
+    /*Test Case: Validate the functionality of the location search feature
+    *1.Navigate to the MBUSA website.
+    *2.Locate and click on the "Find a Dealer" or "Locations" section
+    *3.Verify that the location search page is displayed.
+    *4.Enter the desired location details by providing the city, state, ZIP code, or any other relevant information in the search field.
+    *5.Click on the search button.
+    *6.Verify that the search results are displayed.
+    * 7.Click on collision center button
+    * 8.Check if the search results list contains relevant locations.
+     */
     @Test(priority=5,groups={"BAT"},dataProvider = "findDealerLocationDataProvider")
     public void testFindDealer(String zipCode){
         HomePage homePage=new HomePage();
