824509fc4b14caedc8634103451c8f561a3881ea
Dalal-QA <sadouni,dalal@gmail.com>
2023-07-11 17:04:06 -0400

    test search completed

--- com.booking/src/main/java/homepage/HomePage.java
+++ com.booking/src/main/java/homepage/HomePage.java
@@ -21,6 +21,27 @@ public class HomePage extends BasePage {
     @FindBy(xpath="//span[.='Sign out']")
     public WebElement signOutButton;
 
+    @FindBy(xpath="//span[.='Flights']")
+    public WebElement flightsButton;
+
+    @FindBy(xpath="//button[3]/div[@class='css-ztjjo9']//span[@class='Text-module__root--variant-body_2___-3fYM Text-module__root--color-disabled___tERfD']")
+    public WebElement whereToInputField;
+    @FindBy(xpath="//input[@class='css-1ejj2j9']")
+    public WebElement airportInputField;
+    @FindBy(xpath = "//ul[@class='css-1eonra']//span[@class='css-3cj1dx']/span[contains(.,'CFU Corfu International Airport')]")
+    public WebElement selectedAirport;
+    @FindBy(css="[placeholder='Depart – Return']")
+    public WebElement departReturnField;
+    @FindBy(xpath = "//div[@class='Calendar-module__content___XsvtK']//tr[2]//span[@class='Calendar-module__date___FlMKj']/span[1]/span[.='11']")
+    public WebElement firstDate;
+    @FindBy(xpath="//div[@class='Calendar-module__content___XsvtK']//tr[2]//span[@class='Calendar-module__date___FlMKj']/span[1]/span[.='12']")
+    public WebElement secondDate;
+
+    @FindBy(xpath = "//span[.='Search']")
+    public WebElement searchButton;
+
+    @FindBy(xpath="//div[.='Showing 380 results']")
+    public WebElement searchResults;
 
     public RegistrationPage clickOnRegisterButton(){
         safeClickOnElement(registerButton);
@@ -33,4 +54,42 @@ public class HomePage extends BasePage {
         return signOutButton;
     }
 
+    public void clickOnFlightButton(){
+        safeClickOnElement(flightsButton);
+    }
+    public void clickOnWhereToGoButton(){
+        safeClickOnElement(whereToInputField);
+    }
+    public void enterDestinationName(String countryName){
+        sendKeysToElement(airportInputField,countryName);
+    }
+    public void clickOnSelectedAirport(){
+        safeClickOnElement(selectedAirport);
+    }
+
+    public void clickOnDepartReturnButton(){
+        safeClickOnElement(departReturnField);
+    }
+
+    public void clickOnFirstDate(){
+        safeClickOnElement(firstDate);
+    }
+    public void clickOnSecondDate(){
+        safeClickOnElement(secondDate);
+    }
+
+    public void clickOnSearchButton(){
+        safeClickOnElement(searchButton);
+    }
+
+    public void doSearchFlights(String countryName){
+        clickOnFlightButton();
+        clickOnWhereToGoButton();
+        enterDestinationName(countryName);
+        clickOnSelectedAirport();
+        clickOnDepartReturnButton();
+        clickOnFirstDate();
+        clickOnSecondDate();
+        clickOnSearchButton();
+    }
 }
