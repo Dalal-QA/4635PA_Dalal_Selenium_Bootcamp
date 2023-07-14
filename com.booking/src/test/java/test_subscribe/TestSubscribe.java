552fd250da5246c7a731859a085376ba328d1042
Dalal-QA <sadouni,dalal@gmail.com>
2023-07-14 11:19:53 -0400

    test documented

--- com.booking/src/test/java/test_subscribe/TestSubscribe.java
+++ com.booking/src/test/java/test_subscribe/TestSubscribe.java
@@ -8,6 +8,15 @@ import utils.ExcelData;
 
 public class TestSubscribe extends BasePage {
 
+    /*Test Case: Subscribe on Booking.com
+    *1.Navigate to the Booking.com homepage.
+    *2.Locate the subscription or newsletter signup section on the homepage. It is typically displayed as a form or a subscription widget.
+    *3.Fill in the required information in the subscription form, including:
+          Email address: Enter a valid email address to receive updates and newsletters
+    *4.Click on the "Subscribe" or "Sign Up" button to submit the subscription form
+    *5.Check the provided email address for a subscription confirmation email from Booking.com
+     */
+
     @Test(priority = 1, groups={"BAT"}, dataProvider = "subscribeDataProvider")
     public void testDoSubscribeEmail(String email) {
         HomePage homePage = new HomePage();
