ff494abcb257f84023ef2e552d034ae4a4da5cde
Dalal-QA <sadouni,dalal@gmail.com>
2023-07-14 10:39:24 -0400

    test documented

--- com.streeteasy/src/test/java/test_filtersearch/TestSearch.java
+++ com.streeteasy/src/test/java/test_filtersearch/TestSearch.java
@@ -6,7 +6,14 @@ import org.testng.annotations.Test;
 import searchpage.SearchPage;
 
 public class TestSearch extends BasePage {
-
+/*Test Case:  Test Filter your search functionality on StreetEasy
+*1.Open your web browser and go to the StreetEasy website (www.streeteasy.com).
+*3.Click on "Buy" link
+*4.Select the city where you want to make your purchase.
+*5.Select the type of desired property
+*7.Verify that the displayed result is relevant to your filter.
+*
+ */
     @Test(priority = 1,groups={"BAT"})
     public void testDoFilter(){
         HomePage homePage=new HomePage();
