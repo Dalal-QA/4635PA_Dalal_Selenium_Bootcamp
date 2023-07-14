44d4b4ca637a0152466f03f5e8e96426c05506be
Dalal-QA <sadouni,dalal@gmail.com>
2023-07-13 22:55:16 -0400

    test attraction help center added

--- com.booking/src/test/java/test_filterattraction/TestFilterAttraction.java
+++ com.booking/src/test/java/test_filterattraction/TestFilterAttraction.java
@@ -7,6 +7,15 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestFilterAttraction extends BasePage {
+    /*Test Case:Filter Attractions on Booking.com
+    *1.Navigate to booking.com.
+    *2.Click on the "Attractions" link.
+    *3.Select a category filter from the available options.
+    *4.Verify that the attractions are filtered based on the selected category.
+    *5.Select a price range filter (e.g., "$$" - moderate) from the available options
+    *6.Verify that the attractions are filtered based on the selected price range.
+    * 7.Verify that the attractions are sorted according to the selected sorting option.
+     */
 
     @Test(priority=5,groups={"BAT"})
     public void testDoFilterAttraction(){
--- com.booking/src/test/java/test_helpcenter/TestHelpCenter.java
+++ com.booking/src/test/java/test_helpcenter/TestHelpCenter.java
@@ -7,6 +7,15 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestHelpCenter extends BasePage {
+
+     /*Test Case:Help center on Booking.com
+    *1.Navigate to booking.com.
+    *2.Click on the "Attractions" link.
+    *3.Click on "Attractions help center" icon.
+    *4.Click on "How could the coronavirus affect my visit" drop down
+    *5.Click "Here" link
+    * 6.Verify that you are able to access all the restrictions and requirements info.
+     */
     @Test(priority = 1, groups={"BAT"})
     public void testHelpCenter(){
 
